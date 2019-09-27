037d4ebdcfd51274444bea741123f25b725f6117
amandamarkmann <amanda.markmann@live.se>
2019-09-27 09:58:41 +0200

    Second commit; created AppUser updated version in pom to 2.1.6

---
+++ workshopspringboot/src/main/java/lexicon/se/workshopspringboot/entity/AppUser.java
@@ -0,0 +1,14 @@
+package lexicon.se.workshopspringboot.entity;
+
+public class AppUser {
+	
+	private int id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+
+}
